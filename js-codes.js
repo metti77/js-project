cf8d3ee295f66d4f6dea44550fa4eb77ffa282ec
metti <mehdiasghari615@gmail.com>
2023-08-13 19:29:33 +0330

    third

--- js-codes.js
+++ js-codes.js
@@ -1,10 +1,46 @@
 console.log("connected...");
 
-var w , s , g;
+//var w , s , g;
+//var number=12; 
+//var name="ali";
+//var null=23 + amir ; multiple 
+
 
 w=5 ;
 s=85;
 g=w*s;
 
-document.write(g);
+//document.write(g);
 //window.alert("heey");
+
+var a={name1:"ali",name2:"mamad"};
+document.write(a.name1+"  "+a.name2);
+//document.write(a.name2); var object  (upside)
+var a=(10);
+var b=(20);
+
+var iscomplete=true;  //boolean  
+var isdelete=0;
+var isequal=(a==b);
+
+document.write(isequal);
+
+//arrays
+var colors=["red","blue","pink"];
+//var names= array("ali","hosien");
+
+var ali=[32,52,85,"koln",true];
+console.log(ali [4]);
+
+var colors3=["black","white","yellow"];
+//colors2.reverse();
+//colors2.shift();
+//colors2.unshift("vivi");
+//colors2.pop();
+//colors2.push("jiji");
+
+//var result=colors3.indexOf("white");
+var n =colors3.join( " - " );
+
+console.log(n);
+
